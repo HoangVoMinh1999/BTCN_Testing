3551805ac10c4ad74914c9e07c2f006174d0eea7
Hoang Vo Minh <hoangvm.hiephoa@gmail.com>
2020-12-28 11:50:36 +0700

    Update travis file

--- src/test/java/BTCN_02/mainTest.java
+++ src/test/java/BTCN_02/mainTest.java
@@ -9,23 +9,23 @@ public class mainTest {
     public void test01() {
         String rs = mainClass.checkTriangle(0, 0, 0);
         String expected = "Không phải tam giác";
-        assertEquals(rs, expected);
+        assertEquals("My error message",rs, expected);
 	}
 	@Test
     public void test02() {
         String rs = mainClass.checkTriangle(2, 3, 4);
         String expected = "Tam giác nhọn";
-        assertEquals(rs, expected);
+        assertEquals("My error message",rs, expected);
 	}	@Test
     public void test03() {
         String rs = mainClass.checkTriangle(3, 4, 5);
         String expected = "Tam giác vuông";
-        assertEquals(rs, expected);
+        assertEquals("My error message",rs, expected);
 	}	@Test
     public void test04() {
         String rs = mainClass.checkTriangle(5, 5, 5);
         String expected = "Tam giác đều";
-        assertEquals(rs, expected);
+        assertEquals("My error message",rs, expected);
 	}
 
 }
